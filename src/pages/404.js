49d4d5ab341980c86165595761240775daade89d
takanori-hayashi <mail@m.tknrh.me>
2020-08-18 13:43:17 +0900

    404ページ

--- src/pages/404.js
+++ src/pages/404.js
@@ -10,8 +10,8 @@ const NotFoundPage = ({ data, location }) => {
   return (
     <Layout location={location} title={siteTitle}>
       <SEO title="404: Not Found" />
-      <h1>Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <h1>ページが見つかりません</h1>
+      <p>お探しのページは、移動または削除された可能性があります。</p>
     </Layout>
   )
 }
